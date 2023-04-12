fa3c84c02f46a6853eb6cb066211db46076a8203
kirat14 <moumini.tarik@gmail.com>
2023-04-12 07:11:04 -0500

    updates

--- lab5/script.js
+++ lab5/script.js
@@ -61,6 +61,109 @@ function findLongestWord(words) {
   }
 
 
+  function filterWordsByLength(wordsArray, minLength) {
+    return wordsArray.filter(function (word) {
+      return word.length > minLength;
+    });
+  }
+  
+  function sumOfSquares(numbersArray) {
+    return numbersArray.reduce(function (sum, number) {
+      return sum + number * number;
+    }, 0);
+  }
+  
+  function printOddNumbersUsingForOf(numbersArray) {
+    for (let number of numbersArray) {
+      if (number % 2 !== 0) {
+        console.log(number);
+      }
+    }
+  }
+  
+  function printOddNumbers(numbersArray) {
+    numbersArray.forEach(function (number) {
+      if (number % 2 !== 0) {
+        console.log(number);
+      }
+    });
+  }
+  
+  function sumOfSquaresOfEvenNumbers(numbersArray) {
+    return numbersArray
+      .filter(function (number) {
+        return number % 2 === 0;
+      })
+      .reduce(function (sum, number) {
+        return sum + number * number;
+      }, 0);
+  }
+  
+  function findSecondLargestUsingForOf(numbersArray) {
+    let largest = numbersArray[0];
+    let secondLargest = numbersArray[0];
+  
+    for (let num of numbersArray) {
+      if (num > largest) {
+        secondLargest = largest;
+        largest = num;
+      } else if (num > secondLargest && num !== largest) {
+        secondLargest = num;
+      }
+    }
+  
+    return secondLargest;
+  }
+  
+  function findSecondLargest(numbersArray) {
+    let largest = numbersArray[0];
+    let secondLargest = numbersArray[0];
+  
+    for (let i = 1; i < numbersArray.length; i++) {
+      if (numbersArray[i] > largest) {
+        secondLargest = largest;
+        largest = numbersArray[i];
+      } else if (numbersArray[i] > secondLargest && numbersArray[i] !== largest) {
+        secondLargest = numbersArray[i];
+      }
+    }
+  
+    return secondLargest;
+  }
+  
+  function findSecondLargestUsingReduce(numbersArray) {
+    const [largest, secondLargest] = numbersArray.reduce(
+      ([max, secondMax], current) => {
+        if (current > max) {
+          return [current, max];
+        } else if (current > secondMax && current !== max) {
+          return [max, current];
+        }
+        return [max, secondMax];
+      },
+      [-Infinity, -Infinity]
+    );
+    return secondLargest;
+  }
+  
+  function printFibonacciSequence(length, firstTerm, secondTerm) {
+    let fib = [firstTerm, secondTerm];
+    for (let i = 2; i < length; i++) {
+      fib[i] = fib[i - 1] + fib[i - 2];
+    }
+    console.log(fib.join(", "));
+  }
+  
+  function sumOfNumbersGreaterThanTwenty(numbersArray) {
+    return numbersArray.filter((num) => num > 20).reduce((acc, num) => acc + num, 0);
+  }
+  
+  const getWordsWithLengthAndLetter = function (wordsArray) {
+    return wordsArray.filter((str) => str.length >= 5 && str.includes("a"));
+  };
+  
+
+
 
 /* Extra */
   const a = [1,3,5,3,3];
